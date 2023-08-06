37f026a37ab6604bd449353886ac42b7c915b4c5
chck <shimekiri.today@gmail.com>
2023-08-06 18:54:43 +0900

    Organize contents

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,78 +1,74 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
+export const name = "chck"
+export const designation = "Research Engineer"
+export const location = "Tokyo, Japan"
 export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const website = "https://chck.dev"
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about = "I've spent three years as a web engineer and six years as a research engineer at an online advertising company. My research interests lie in computer vision, natural language processing, and machine learning. The immediate mission is to scale R&D in my company via engineering such as MLOps and ResearchOps."
 
 // Work Experience --> #work section
 export const work = [
 	{
-		from: "2001",
+		from: "2014",
 		to: "Present",
-		role: "Safety Inspector",
-		org: "Quahog Safety Department",
-		org_url: "https://quahog.gov/safety",
-		location: "Quahog, Rhode Island",
-		skills: "Safety Inspection, Risk Assessment, Emergency Response"
+		role: "Research Engineer",
+		org: "CyberAgent, Inc.",
+		org_url: "https://www.cyberagent.co.jp/",
+		location: "Tokyo, Japan",
+		skills: "Python, Machine Learning, GCP, AWS"
 	}
 ]
 
 // Projects --> #project section
 export const projects = [
-	{
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
-	},
-	{
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
-	},
-	{
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
-	}
+	// {
+	// 	title: "Project Griffin",
+	// 	description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
+	// 	url: "https://projectgriffin.com",
+	// 	tech: "Safety Equipment, Training Programs, Community Outreach",
+	// },
+	// {
+	// 	title: "Family Safety Campaign",
+	// 	description: "An initiative to promote safety awareness among families in Quahog.",
+	// 	url: "https://safefamilyquahog.com",
+	// 	tech: "Workshops, Educational Material, Public Events",
+	// },
+	// {
+	// 	title: "Road Safety Initiative",
+	// 	description: "A campaign to enhance road safety and reduce accidents in Quahog.",
+	// 	url: "https://roadsafetyquahog.com",
+	// 	tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+	// }
 ]
 
 // Contact --> #contact section
 export const contact = [
 	{
 		source_name: "Email",
-		source: "peter.griffin@example.com",
-	},
-	{
-		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
+		source: "shimekiri.today@gmail.com",
 	},
 	{
 		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
+		source: "https://www.linkedin.com/in/chck",
 	},
 ]
 
 // Blogs --> #blogs section
 export const blogs = [
-	{
-		date: "12-July, 2023",
-		url: "https://medium.com/",
-		title: "Adventures in Safety Inspection",
-		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-		publisher: "Quahog Times",
-	},
-	{
-		date: "25-June, 2023",
-		url: "https://medium.com/",
-		title: "Top 10 Safety Tips for Families",
-		description: "Learn the best safety practices for families to ensure a secure environment.",
-		publisher: "Safety Monthly",
-	}
+	// {
+	// 	date: "12-July, 2023",
+	// 	url: "https://medium.com/",
+	// 	title: "Adventures in Safety Inspection",
+	// 	description: "Join me in my humorous escapades as a safety inspector in Quahog.",
+	// 	publisher: "Quahog Times",
+	// },
+	// {
+	// 	date: "25-June, 2023",
+	// 	url: "https://medium.com/",
+	// 	title: "Top 10 Safety Tips for Families",
+	// 	description: "Learn the best safety practices for families to ensure a secure environment.",
+	// 	publisher: "Safety Monthly",
+	// }
 ]
