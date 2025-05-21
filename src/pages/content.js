6299147e7c90462eb0c0bd4c4afba51b8c61bd57
chck <shimekiri.today@gmail.com>
2025-05-21 19:10:56 +0900

    feat: add writings since 2025-05

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,162 +1,200 @@
 // Personal Information --> #hero section
-export const name = "chck"
-export const designation = "Research Engineer"
-export const location = "Tokyo, Japan"
-export const pronouns = "he/him"
-export const website = "https://chck.dev"
+export const name = "chck";
+export const designation = "Research Engineer";
+export const location = "Tokyo, Japan";
+export const pronouns = "he/him";
+export const website = "https://chck.dev";
 
 // About
-export const about = "I've spent three years as a web engineer and six years as a research engineer at an online advertising company. My research interests lie in computer vision, natural language processing, and machine learning. The immediate mission is to scale R&D in my company via engineering such as MLOps and ResearchOps."
+export const about =
+  "I've spent three years as a web engineer and six years as a research engineer at an online advertising company. My research interests lie in computer vision, natural language processing, and machine learning. The immediate mission is to scale R&D in my company via engineering such as MLOps and ResearchOps.";
 
 // Work Experience --> #work section
 export const work = [
-	{
-		from: "2014",
-		to: "Present",
-		role: "Research Engineer",
-		org: "CyberAgent, Inc.",
-		org_url: "https://www.cyberagent.co.jp/",
-		location: "Tokyo, Japan",
-		skills: "Python, Machine Learning, GCP, AWS"
-	}
-]
+  {
+    from: "2014",
+    to: "Present",
+    role: "Research Engineer",
+    org: "CyberAgent, Inc.",
+    org_url: "https://www.cyberagent.co.jp/",
+    location: "Tokyo, Japan",
+    skills: "Python, Machine Learning, GCP, AWS",
+  },
+];
 
 // Projects --> #project section
 export const projects = [
-	{
-		from: "2014",
-		to: "2015",
-		title: "Backend Engineering (GameLogic/Audience, Smalgo)",
-		description: "An online advertising system as demand-side platform (DSP) to focus on mobile game publishers.",
-		url: "https://www.slideshare.net/Satully/adtechstudio-20140301",
-		tech: "Java, MySQL, Redis, Ruby on Rails, Angular, AWS",
-	},
-	{
-		from: "2015",
-		to: "2016",
-		title: "Backend Engineering (Game Tailor)",
-		description: "A dynamic retargeting ad product for smartphone apps. Tailor-made ads generated based on each game user status such as game level, selected characters and log in frequency will be served to worldwide game users through social medias.",
-		url: "https://rtbsquare.work/archives/6067",
-		tech: "Scala, MySQL, Aerospike, Apache Spark, RedShift, AWS",
-	},
-	{
-		from: "2016",
-		to: "2017",
-		title: "Backend Engineering and Data Science (iXamDrive)",
-		description: "An operation platform for in-feed advertising. It can publish dynamic ads social media such as Facebook, Twitter, and LINE as official partner.",
-		url: "https://rtbsquare.work/archives/13423",
-		tech: "Scala, Java, Finagle, Python, TensorFlow, AWS, GCP",
-	},
-	{
-		from: "2017",
-		to: "Present",
-		title: "Research Engineering (AI Lab)",
-		description: "Scale R&D in my company via engineering such as MLOps and ResearchOps",
-		url: "https://cyberagent.ai/ailab/",
-		tech: "NLP, Computer Vision, Python, TensorFlow, PyTorch, TypeScript, Kubernetes, Terraform, GCP, AWS",
-	}
-]
+  {
+    from: "2014",
+    to: "2015",
+    title: "Backend Engineering (GameLogic/Audience, Smalgo)",
+    description:
+      "An online advertising system as demand-side platform (DSP) to focus on mobile game publishers.",
+    url: "https://www.slideshare.net/Satully/adtechstudio-20140301",
+    tech: "Java, MySQL, Redis, Ruby on Rails, Angular, AWS",
+  },
+  {
+    from: "2015",
+    to: "2016",
+    title: "Backend Engineering (Game Tailor)",
+    description:
+      "A dynamic retargeting ad product for smartphone apps. Tailor-made ads generated based on each game user status such as game level, selected characters and log in frequency will be served to worldwide game users through social medias.",
+    url: "https://rtbsquare.work/archives/6067",
+    tech: "Scala, MySQL, Aerospike, Apache Spark, RedShift, AWS",
+  },
+  {
+    from: "2016",
+    to: "2017",
+    title: "Backend Engineering and Data Science (iXamDrive)",
+    description:
+      "An operation platform for in-feed advertising. It can publish dynamic ads social media such as Facebook, Twitter, and LINE as official partner.",
+    url: "https://rtbsquare.work/archives/13423",
+    tech: "Scala, Java, Finagle, Python, TensorFlow, AWS, GCP",
+  },
+  {
+    from: "2017",
+    to: "Present",
+    title: "Research Engineering (AI Lab)",
+    description:
+      "Scale R&D in my company via engineering such as MLOps and ResearchOps",
+    url: "https://cyberagent.ai/ailab/",
+    tech: "NLP, Computer Vision, Python, TensorFlow, PyTorch, TypeScript, Kubernetes, Terraform, GCP, AWS",
+  },
+];
 
 // Education --> #education section
 export const education = [
-//	{
-//		from: "1995",
-//		to: "1999",
-//		institute: "Quahog High School",
-//		course: "High School Diploma",
-//		location: "Quahog, Rhode Island"
-//	},
-//	{
-//		from: "1999",
-//		to: "2003",
-//		institute: "Quahog Community College",
-//		course: "Bachelor of Science in Safety Management",
-//		location: "Quahog, Rhode Island"
-//	},
-//	{
-//		from: "2003",
-//		to: "2005",
-//		institute: "Safety University",
-//		course: "Master of Science in Safety Engineering",
-//		location: "Boston, Massachusetts"
-//	}
-]
+  //	{
+  //		from: "1995",
+  //		to: "1999",
+  //		institute: "Quahog High School",
+  //		course: "High School Diploma",
+  //		location: "Quahog, Rhode Island"
+  //	},
+  //	{
+  //		from: "1999",
+  //		to: "2003",
+  //		institute: "Quahog Community College",
+  //		course: "Bachelor of Science in Safety Management",
+  //		location: "Quahog, Rhode Island"
+  //	},
+  //	{
+  //		from: "2003",
+  //		to: "2005",
+  //		institute: "Safety University",
+  //		course: "Master of Science in Safety Engineering",
+  //		location: "Boston, Massachusetts"
+  //	}
+];
 
 // Contact --> #contact section
 export const contact = [
-	{
-		source_name: "Email",
-		source: "mailto:shimekiri.today@gmail.com",
-	},
-	{
-		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/chck",
-	},
-]
+  {
+    source_name: "Email",
+    source: "mailto:shimekiri.today@gmail.com",
+  },
+  {
+    source_name: "LinkedIn",
+    source: "https://www.linkedin.com/in/chck",
+  },
+];
 
 // Certifications --> #certificate section
 export const certificates = [
-//	{
-//		issued: "2005",
-//		title: "Certified Safety Inspector",
-//		org: "National Safety Association",
-//		url: "https://certi.com/url",
-//		description: "Certification in safety inspection and risk assessment.",
-//	},
-//	{
-//		issued: "2006",
-//		title: "Emergency Response Training",
-//		org: "Quahog Safety Department",
-//		url: "https://certi.com/url",
-//		description: "Training in handling emergency situations and crisis management.",
-//	},
-//	{
-//		issued: "2007",
-//		title: "Safety Leadership Workshop",
-//		org: "Safety Leaders International",
-//		url: "https://certi.com/url",
-//		description: "Leadership training in safety management and communication.",
-//	}
-]
+  //	{
+  //		issued: "2005",
+  //		title: "Certified Safety Inspector",
+  //		org: "National Safety Association",
+  //		url: "https://certi.com/url",
+  //		description: "Certification in safety inspection and risk assessment.",
+  //	},
+  //	{
+  //		issued: "2006",
+  //		title: "Emergency Response Training",
+  //		org: "Quahog Safety Department",
+  //		url: "https://certi.com/url",
+  //		description: "Training in handling emergency situations and crisis management.",
+  //	},
+  //	{
+  //		issued: "2007",
+  //		title: "Safety Leadership Workshop",
+  //		org: "Safety Leaders International",
+  //		url: "https://certi.com/url",
+  //		description: "Leadership training in safety management and communication.",
+  //	}
+];
 
 // Blogs --> #blogs section
 export const blogs = [
-//	{
-//		date: "12-July, 2023",
-//		url: "https://medium.com/",
-//		title: "Adventures in Safety Inspection",
-//		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-//		publisher: "Quahog Times",
-//	},
-//	{
-//		date: "25-June, 2023",
-//		url: "https://medium.com/",
-//		title: "Top 10 Safety Tips for Families",
-//		description: "Learn the best safety practices for families to ensure a secure environment.",
-//		publisher: "Safety Monthly",
-//	}
-]
+  {
+    date: "17-Apr, 2025",
+    url: "https://cyberagent.ai/blog/research/20228/",
+    title: "Skill Up Boot Camp for Research Scientists",
+    description:
+      "Skill up onboarding for researchers were held and materials are available.",
+  },
+  {
+    date: "25-Dec, 2024",
+    url: "https://zenn.dev/chck/articles/475dc1dd6590d4",
+    title: "User Embeddings from their skill set",
+    description:
+      "Attempts to reveal people skill sets based on their text (e.g., slack chats, source codes, papers, etc.).",
+  },
+  {
+    date: "13-Oct, 2021",
+    url: "https://cyberagent.ai/blog/research/15272/",
+    title: "Easy-to-use experiment management hands-on with MLflow in GKE",
+    description:
+      "Experimental management systems using kubernetes clusters for researchers.",
+  },
+  {
+    date: "1-Jun, 2020",
+    url: "https://cyberagent.ai/blog/research/12898/",
+    title: "MLOps2020: Start Small, Grow Big",
+    description:
+      "The effective use of MLOps tools such as Hydra, Sacred, MLflow, Kedro, Metaflow, Kubeflow, and Optuna is explained with a comparison of their features.",
+  },
+  {
+    date: "5-Feb, 2019",
+    url: "https://cyberagent.ai/blog/research/10257/",
+    title: "Tweet-based matching system technologies",
+    description:
+      "An installation work that extracts user features from their tweet and pre-questionnaires to realize a dialogue in an Augmented Reality space.",
+  },
+  //	{
+  //		date: "12-July, 2023",
+  //		url: "https://medium.com/",
+  //		title: "Adventures in Safety Inspection",
+  //		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
+  //	},
+  //	{
+  //		date: "25-June, 2023",
+  //		url: "https://medium.com/",
+  //		title: "Top 10 Safety Tips for Families",
+  //		description: "Learn the best safety practices for families to ensure a secure environment.",
+  //	}
+];
 
 // Achievements --> #achievement section
 export const achievements = [
-//	{
-//		title: "Safety Hero Award",
-//		year: "2020",
-//		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
-//	},
-//	{
-//		title: "Safety Innovation Prize",
-//		year: "2018",
-//		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-//	},
-//	{
-//		title: "Community Safety Ambassador",
-//		year: "2015",
-//		decription: "Honored for promoting safety awareness and education in the Quahog community."
-//	},
-//	{
-//		title: "Safety Campaign Excellence",
-//		year: "2013",
-//		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
-//	}
-]
+  //	{
+  //		title: "Safety Hero Award",
+  //		year: "2020",
+  //		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+  //	},
+  //	{
+  //		title: "Safety Innovation Prize",
+  //		year: "2018",
+  //		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
+  //	},
+  //	{
+  //		title: "Community Safety Ambassador",
+  //		year: "2015",
+  //		decription: "Honored for promoting safety awareness and education in the Quahog community."
+  //	},
+  //	{
+  //		title: "Safety Campaign Excellence",
+  //		year: "2013",
+  //		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+  //	}
+];
